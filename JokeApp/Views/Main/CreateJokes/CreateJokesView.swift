3843ca73edbe4038c6f213b9cf0482da37874af4
jenni2503 <june-jenny@hotmail.com>
2024-02-25 22:54:57 +0100

    create jokes view

---
+++ JokeApp/Views/Main/CreateJokes/CreateJokesView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateJokesView.swift
+//  JokeApp
+//
+//  Created by Jennifer Luu on 25/02/2024.
+//
+
+import SwiftUI
+
+struct CreateJokesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateJokesView()
+}
